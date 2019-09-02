7e178f1d6c151746fefb3cde2891cdb6833efd75
Araf Hossain <arafh13@gmail.com>
2019-09-02 17:44:33 -0400

    Styled videoList and items

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -3,10 +3,10 @@ import VideoItem from './VideoItem'
 
 let VideoList = props => {
   let renderedList = props.videos.map((video) => {
-    return <VideoItem video={video}/>;
+    return <VideoItem video={video} onVideoSelect={props.onVideoSelect}/>;
   });
 
-  return <div>{renderedList}</div>;
+  return <div className="ui relaxed divided list">{renderedList}</div>;
 };
 
 export default VideoList;
