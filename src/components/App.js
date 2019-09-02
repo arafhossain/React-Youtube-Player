866a3503d9933549a62c6f467c5fce97e54d7c4a
Araf Hossain <arafh13@gmail.com>
2019-09-02 16:06:31 -0400

    Craeted onSubmit API search
    Set state to youtube results

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import SearchBar from './SearchBar'
+import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
 
 class App extends React.Component {
-  onTermSubmit = (term) => {
-    
+  state = { videos: []};
+
+  onTermSubmit = async term => {
+    let response = await youtube.get('/search', {
+      params: {
+        q: term
+      }
+    });
+    this.setState({videos: response.data.items});
   }
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit}/>
+        I have {this.state.videos.length} videos.
       </div>
     )
   }
