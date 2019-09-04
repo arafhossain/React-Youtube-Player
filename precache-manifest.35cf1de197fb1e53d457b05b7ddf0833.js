d250e85fcb6edcc871496fe316063d29f1c12914
Araf Hossain <arafh13@gmail.com>
2019-09-04 12:37:37 -0400

    Updates

---
+++ precache-manifest.35cf1de197fb1e53d457b05b7ddf0833.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a20595e24b36992c57430cc6050e253",
+    "url": "/Youtube-Player/index.html"
+  },
+  {
+    "revision": "a76a70c1ecc72cba11d7",
+    "url": "/Youtube-Player/static/css/main.ca161dc0.chunk.css"
+  },
+  {
+    "revision": "8bd38698bc9fd9b46a23",
+    "url": "/Youtube-Player/static/js/2.3e58d82b.chunk.js"
+  },
+  {
+    "revision": "a76a70c1ecc72cba11d7",
+    "url": "/Youtube-Player/static/js/main.18c80ff3.chunk.js"
+  },
+  {
+    "revision": "a513ef53fff2844f00bc",
+    "url": "/Youtube-Player/static/js/runtime~main.8e1e86e3.js"
+  }
+]);
